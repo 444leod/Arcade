f184cfea375906e8612c67b97de4e4a9f8be5c78
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-13 13:16:07 +0100

    fix(file name): changed file name for compilation.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** main
+** File description:
+** main
+*/
+
+int main(int argc, char *argv[], char *env[])
+{
+    (void)argc;
+    (void)argv;
+    (void)env;
+    return 0;
+}
