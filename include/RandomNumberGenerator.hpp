74ee817287efe08770706b993f02238348ddcbbf
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-22 20:41:10 +0100

    fix(rng): random correctly working now.

--- include/RandomNumberGenerator.hpp
+++ include/RandomNumberGenerator.hpp
@@ -9,18 +9,32 @@
 
 #include <random>
 #include <ctime>
+#include <functional>
 
-class RandomNumberGenerator {
-public:
-    RandomNumberGenerator() : gen(std::random_device{}()), distrib(0, 99) {
-        gen.seed(std::time(nullptr));
-    }
+namespace rng {
+    class RandomNumberGenerator {
+    public:
+        RandomNumberGenerator() : gen(std::random_device{}()), distrib(0, 1000) {
+            gen.seed(std::time(nullptr));
+        }
 
-    int generate() {
-        return distrib(gen);
-    }
+        int generate() {
+            return distrib(gen);
+        }
 
-private:
-    std::mt19937 gen;
-    std::uniform_int_distribution<> distrib;
-};
+        int generate(int lower_bound, int upper_bound) {
+            std::uniform_int_distribution<> distrib(lower_bound, upper_bound);
+            return distrib(gen);
+        }
+
+    private:
+        std::mt19937 gen;
+        std::uniform_int_distribution<> distrib;
+    };
+
+        std::function<int(int, int)> rand = [] (int lower_bound = 0, int upper_bound = 100) {
+            RandomNumberGenerator rng_instance;
+            return rng_instance.generate(lower_bound, upper_bound);
+        };
+}
+
