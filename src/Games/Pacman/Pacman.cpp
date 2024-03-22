45ca4327fb786cf8033aff37816cf0f6c2bfb7dd
Léo DUMONT <leo.dumont@epitech.eu>
2024-03-22 13:38:05 +0100

    feat(ghosts handling): added ghost handling to main pacman loop.

--- src/Games/Pacman/Pacman.cpp
+++ src/Games/Pacman/Pacman.cpp
@@ -9,6 +9,11 @@
 #include "MapHandler.hpp"
 #include "Player.hpp"
 #include "IEntity.hpp"
+#include "ghosts/Blinky.hpp"
+#include "ghosts/Pinky.hpp"
+#include "ghosts/Inky.hpp"
+#include "ghosts/Clyde.hpp"
+#include "RandomNumberGenerator.hpp"
 
 #include <iostream>
 #include <cstdlib>
@@ -17,9 +22,6 @@
 #include <map>
 #include <vector>
 #include <memory>
-
-// using pacman::MapHandler;
-// using pacman::player::Player;
 
 class MyGame : public arc::IGame {
 public:
@@ -35,13 +37,7 @@ public:
         lib.display().setFramerate(60);
         lib.display().setTileSize(32);
 
-        // Textures
         arc::TextureSpecification spec;
-        spec.textual.character = 'P';
-        spec.textual.color = {255, 255, 0, 255};
-        spec.graphical = arc::TextureImage{"assets/tileset.png", arc::Rect<uint32_t>{0, 0, 16, 16}};
-        lib.textures().load("player", spec);
-
         spec.textual.character = '.';
         spec.textual.color = {255, 255, 0, 255};
         spec.graphical = arc::TextureImage{"assets/pacman/board.png", arc::Rect<uint32_t>{1024, 0, 32, 32}};
@@ -70,13 +66,29 @@ public:
         arc::FontSpecification text {
             .color = arc::Color {200, 200, 200, 255},
             .size = 16,
-            .path = "assets/regular.ttf"
+            .path = "assets/pacman/emulogic.ttf"
         };
-        lib.fonts().load("font", text);
+        lib.fonts().load("emulogic", text);
 
         lib.display().setWidth(_mapHandler.getMapSizeX());
         lib.display().setHeight(_mapHandler.getMapSizeY() + 5);
         _player->setPos(_mapHandler.getPlayerPos());
+        _player->setPosf(_mapHandler.getPlayerPos());
+        _player->initTextures(lib.textures());
+
+        for (auto& ghost : _ghosts) {
+            ghost->initTextures(lib.textures());
+        }
+
+        RandomNumberGenerator rng;
+        for (auto &ghost : _ghosts) {
+            vec2i ghostPos;
+            do {
+                ghostPos = {rng.generate() % _mapHandler.getMapSizeX(), rng.generate() % _mapHandler.getMapSizeY()};
+            } while (_mapHandler.isTileWalkable(ghostPos) == false);
+            ghost->setPos(ghostPos);
+            ghost->setPosf(ghostPos);
+        }
     }
 
     virtual void onKeyPressed([[maybe_unused]] arc::ILibrary& lib, arc::Key key)
@@ -111,9 +123,35 @@ public:
 
     virtual void update([[maybe_unused]] arc::ILibrary& lib, [[maybe_unused]]float deltaTime)
     {
-        _steps++;
+        _elapsed += deltaTime;
+        _elapsed1 += deltaTime;
+        vec2i playerPos = _player->getPos();
+
         _player->move(deltaTime, _mapHandler);
-        _mapHandler.processPlayerPos(_player->getPos(), _player->getDirection());
+        for (auto& ghost : _ghosts) {
+            ghost->move(deltaTime, _mapHandler);
+        }
+        if (_elapsed1 >= 0.2) {
+            RandomNumberGenerator rng;
+            for (auto& ghost : _ghosts) {
+                pacman::Direction direction = pacman::Direction(rng.generate() % 4);
+                ghost->setMoveQueue({pacman::DirectionToVec2[direction]});
+            }
+            _elapsed1 -= 0.2;
+        }
+        if (_elapsed >= 0.1) {
+            _fruitVisibility = !_fruitVisibility;
+            _playerState = (_playerState + 1) % 2;
+            _elapsed -= 0.1;
+        }
+
+        _player->movef(deltaTime, _mapHandler);
+        std::vector<vec2f> ghostPos;
+        for (auto& ghost : _ghosts) {
+            ghost->movef(deltaTime, _mapHandler);
+            ghostPos.push_back(ghost->getPosf());
+        }
+        _pacmanState = _mapHandler.processPlayerPos(_player->getPos(), _player->getDirection(), _score, ghostPos, _player->getPosf());
         _player->setPos(_mapHandler.getPlayerPos());
     }
 
@@ -126,31 +164,52 @@ public:
         lib.display().clear(arc::Color{0, 0, 0, 0});
 
         for (auto [pos, texture] : _mapHandler.dumpTextures()) {
+            if (texture == "fruit" && !_fruitVisibility)
+                continue;
             lib.display().draw(lib.textures().get(texture), pos.x, pos.y);
         }
 
-        // vec2i initialPos = _player->getPos();
-        // vec2f playerPos = _player->getPosf();
-        // playerPos.x = initialPos.x + playerPos.x;
-        // playerPos.y = initialPos.y + playerPos.y;
-        vec2i playerPos = _player->getPos();
-        lib.display().draw(lib.textures().get("player"), playerPos.x, playerPos.y);
+        std::vector<std::shared_ptr<pacman::IEntity>> entities;
+        entities.push_back(_player);
+        for (auto& ghost : _ghosts) {
+            entities.push_back(ghost);
+        }
+
+        for (auto& entity : entities) {
+            std::string texture = entity->getTexture(_playerState);
+            if (texture.length() == 0)
+                continue;
+            lib.display().draw(lib.textures().get(entity->getTexture(_playerState)), entity->getPosf().x, entity->getPosf().y);
+        }
+
         auto width = lib.display().width();
-        auto textWidth = lib.display().measure(score.str(), lib.fonts().get("font"), 0, 0).width;
+        auto textWidth = lib.display().measure(score.str(), lib.fonts().get("emulogic"), 0, 0).width;
         auto center = (width - textWidth) / 2;
 
-        lib.display().print(score.str(), lib.fonts().get("font"), center, _mapHandler.getMapSizeX() + 3);
-
+        lib.display().print(score.str(), lib.fonts().get("emulogic"), center, _mapHandler.getMapSizeX() + 3);
         lib.display().flush();
+        if (_pacmanState == pacman::player::State::DEAD) {
+            lib.display().close();
+        }
     }
 
 private:
     float _elapsed = 0;
+    float _elapsed1 = 0;
     uint32_t _score = 0;
     uint32_t _steps = 0;
+    bool _fruitVisibility = true;
+    uint32_t _playerState = 0;
+    pacman::player::State _pacmanState = pacman::player::State::ALIVE;
 
     MapHandler _mapHandler;
-    std::unique_ptr<pacman::IEntity> _player = std::make_unique<pacman::player::Player>();
+    std::shared_ptr<pacman::IEntity> _player = std::make_shared<pacman::player::Player>();
+    std::vector<std::shared_ptr<pacman::IEntity>> _ghosts = {
+        std::make_shared<pacman::ghosts::Blinky>(),
+        std::make_shared<pacman::ghosts::Pinky>(),
+        std::make_shared<pacman::ghosts::Inky>(),
+        std::make_shared<pacman::ghosts::Clyde>()
+    };
 };
 
 std::ostream& operator<<(std::ostream& os, const vec2i& vec)
