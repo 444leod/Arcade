14f254858bb5f32f140326567319476b7abef051
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-04-06 21:52:40 +0200

    feat(fighto): added base Vector struct

---
+++ src/Games/Fighto/Vector.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** Vector
+*/
+
+#pragma once
+
+template<typename T>
+struct Vector
+{
+    T x;
+    T y;
+};
+
+#define iVector Vector<int>
+#define fVector Vector<float>
